0aee371936a01b64e66a90089e49b1ca88bd4ff8
dom <dompec07@gmail.com>
2022-10-18 11:12:15 -0400

    penis

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
-local bible = loadstring(game:HttpGet("https://ave.is-a.dev/umbra-premium-free-real"))()
+local bible = loadstring(game:HttpGet("https://ave.is-a.dev/umbra-premium-free-real/bible.lua"))()
 
 for i, v in pairs(bible) do
-	wait()
+	game:GetService("Players").LocalPlayer:Chat(v)
 	print(v)
+	wait(.5)
 end
